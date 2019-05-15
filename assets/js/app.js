eb6496450b72e8f01abf0275e5f30eb01ce8897b
obscure723 <obscure723@yone-lab.com>
2019-05-15 11:37:13 +0900

    no message

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,10 @@ $(function() {
   
   // ddmenuの子要素のli にマウスが入ってきたら
   $('.ddmenu li').mouseenter(function() {
-    // フォーカスされたliの 小要素を
-    $(this).children().slideDown();
+    // フォーカスされたliの 兄弟が持つul を非表示にする
+    $(this).siblings().find('ul').hide();
+    
+    // フォーカスされたliの 小要素を 表示する
+    $(this).children().slideDown('slow');
   });
 })
