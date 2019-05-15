815aeab842ae1527aad3b91513390759294213cb
obscure723 <obscure723@yone-lab.com>
2019-05-15 12:04:17 +0900

    no message

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@ $(function() {
   // ddmenuの子要素のli にマウスが入ってきたら
   $('.ddmenu li').mouseenter(function() {
     // フォーカスされたliの 兄弟が持つul を非表示にする
-    $(this).siblings().find('ul').hide();
+    $(this).siblings().find('ul').slideUp();
     
     // フォーカスされたliの 小要素を 表示する
     $(this).children().slideDown('slow');
@@ -12,9 +12,17 @@ $(function() {
 
   // 画面のどこかがクリックされたら
   $('html').click(function() {
-
     $('.ddmenu ul').slideUp();
-  
   });
+
+
+
+  // 画面がスクロールされたら
+  $(window).scroll(function() {
+    // 画面のスクロールされた量を取得
+    let scrollTop = $(document).scrollTop();
+    console.log(scrollTop);
+    // 矢印の表示・非表示を切り替える
+  })
 
 })
