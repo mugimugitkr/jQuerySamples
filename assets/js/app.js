6e71dc6191c159bef256bcdec81745d4b25decaf
obscure723 <obscure723@yone-lab.com>
2019-05-15 12:22:02 +0900

    no message

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,13 +16,32 @@ $(function() {
   });
 
 
+  // 矢印がクリックされたら
+  $('.arrow').click(function() {
+    // 画面のTopまでスクロールする
+    $('html').animate({ scrollTop: 0 }, 1000);
+  })
+
+
+
 
   // 画面がスクロールされたら
   $(window).scroll(function() {
     // 画面のスクロールされた量を取得
     let scrollTop = $(document).scrollTop();
-    console.log(scrollTop);
+    
+    let arrow = $('.arrow');
+
     // 矢印の表示・非表示を切り替える
+    // スクロール量が500を超えたら、矢印を表示
+    if (scrollTop > 500) {
+      // スクロール量が500を超えたら
+      arrow.fadeIn();
+    } else {
+      // スクロール量が500を超えていない場合
+      arrow.fadeOut();
+    }
+
   })
 
 })
